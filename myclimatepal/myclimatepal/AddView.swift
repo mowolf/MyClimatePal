cec03c6e09a48491200216ee76dfbc42f1141232
KorbinianBstrtr <ga84can@mytum.de>
2020-09-18 23:12:09 +0200

    merge

--- myclimatepal/myclimatepal/AddView.swift
+++ myclimatepal/myclimatepal/AddView.swift
@@ -20,41 +20,41 @@ struct AddView: View {
             Spacer()
             Text("Update your co2 score").font(.title)
             SearchBar(text: $searchText).padding()
-            
-            HStack {
-                Button(action: {
-                    self.Co2State.currentCo2State += 2
-                    // What to perform
-                }) {
-                    Image("car")
-                        .font(.system(size: 60))
-                        .frame(width: iconSize, height: iconSize)
-                }.buttonStyle(PlainButtonStyle())
-                Button(action: {
-                    // What to perform
-                }) {
-                    Image("home")
-                        .font(.system(size: 60))
-                        .frame(width: iconSize, height: iconSize)
-                }.buttonStyle(PlainButtonStyle())
-            }
-            HStack {
-                Button(action: {
-                    // What to perform
-                }) {
-                    Image("food")
-                        .font(.system(size: 60))
-                        .frame(width: iconSize, height: iconSize)
-                }.buttonStyle(PlainButtonStyle())
-                Button(action: {
-                    // What to perform
-                }) {
-                    Image("jumper")
-                        .font(.system(size: 60))
-                        .frame(width: iconSize, height: iconSize)
-                }.buttonStyle(PlainButtonStyle())
-            }
-            Spacer()
+            if searchText == "" {
+                HStack {
+                    Button(action: {
+                        self.co2State.currentCo2State += 2
+                        // What to perform
+                    }) {
+                        Image("car")
+                            .font(.system(size: 60))
+                            .frame(width: iconSize, height: iconSize)
+                    }.buttonStyle(PlainButtonStyle())
+                    Button(action: {
+                        // What to perform
+                    }) {
+                        Image("home")
+                            .font(.system(size: 60))
+                            .frame(width: iconSize, height: iconSize)
+                    }.buttonStyle(PlainButtonStyle())
+                }
+                HStack {
+                    Button(action: {
+                        // What to perform
+                    }) {
+                        Image("food")
+                            .font(.system(size: 60))
+                            .frame(width: iconSize, height: iconSize)
+                    }.buttonStyle(PlainButtonStyle())
+                    Button(action: {
+                        // What to perform
+                    }) {
+                        Image("jumper")
+                            .font(.system(size: 60))
+                            .frame(width: iconSize, height: iconSize)
+                    }.buttonStyle(PlainButtonStyle())
+                }
+                Spacer()
             } else {
                 ListView(items: Co2State.getSearchResults(query: searchText, items: co2State.foodItems))
             }
