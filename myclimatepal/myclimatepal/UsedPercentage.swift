3b03f4ce3ef5cb115d20b92b7bfdf80a1f82ac62
Moritz Wolf <moritz.m.wolf@tum.de>
2020-09-19 20:59:51 +0200

    refactored

---
+++ myclimatepal/myclimatepal/UsedPercentage.swift
@@ -0,0 +1,21 @@
+//
+//  UsedPercentage.swift
+//  myclimatepal
+//
+//  Created by Moritz Wolf on 19.09.20.
+//  Copyright © 2020 myclimatepal. All rights reserved.
+//
+
+import SwiftUI
+
+struct UsedPercentage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UsedPercentage_Previews: PreviewProvider {
+    static var previews: some View {
+        UsedPercentage()
+    }
+}
