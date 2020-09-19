a083f7c16d1ee9feeb1be01da0e01cd2f5daae57
Moritz Wolf <moritz.m.wolf@tum.de>
2020-09-19 13:36:41 +0200

    add graph

---
+++ myclimatepal/myclimatepal/DayGraph.swift
@@ -0,0 +1,21 @@
+//
+//  DayGraph.swift
+//  myclimatepal
+//
+//  Created by Moritz Wolf on 19.09.20.
+//  Copyright © 2020 myclimatepal. All rights reserved.
+//
+
+import SwiftUI
+
+struct DayGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        DayGraph()
+    }
+}
---
+++ myclimatepal/myclimatepal/SnapCarousel.swift
@@ -0,0 +1,9 @@
+//
+//  SnapCarousel.swift
+//  myclimatepal
+//
+//  Created by Moritz Wolf on 19.09.20.
+//  Copyright © 2020 myclimatepal. All rights reserved.
+//
+
+import Foundation
