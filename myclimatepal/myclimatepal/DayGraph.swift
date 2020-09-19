b4ef63bb256122853d5ef30430c73778f292fbff
Moritz Wolf <moritz.m.wolf@tum.de>
2020-09-19 16:30:46 +0200

    add xaxis

--- myclimatepal/myclimatepal/DayGraph.swift
+++ myclimatepal/myclimatepal/DayGraph.swift
@@ -9,6 +9,21 @@
 import SwiftUI
 import SwiftUICharts
 
+let tempGradient = Gradient(colors: [
+  .purple,
+  Color(red: 0, green: 0, blue: 139.0/255.0),
+  .blue,
+  Color(red: 30.0/255.0, green: 144.0/255.0, blue: 1.0),
+  Color(red: 0, green: 191/255.0, blue: 1.0),
+  Color(red: 135.0/255.0, green: 206.0/255.0, blue: 250.0/255.0),
+  .green,
+  .yellow,
+  .orange,
+  Color(red: 1.0, green: 140.0/255.0, blue: 0.0),
+  .red,
+  Color(red: 139.0/255.0, green: 0.0, blue: 0.0)
+])
+
 struct DayGraph: View {
     @EnvironmentObject var co2State: Co2State
 
---
+++ myclimatepal/myclimatepal/LineChart/MagnifierRect.swift
@@ -0,0 +1,41 @@
+//
+//  MagnifierRect.swift
+//  
+//
+//  Created by Samu András on 2020. 03. 04..
+//
+
+import SwiftUI
+
+public struct MagnifierRect: View {
+    @Binding var currentNumber: Double
+    var valueSpecifier:String
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    public var body: some View {
+        ZStack{
+		VStack{
+            Text("\(self.currentNumber, specifier: valueSpecifier)").font(.system(size: 18, weight: .bold))
+		Text("kg Co2").font(.system(size: 15))
+}
+                .offset(x: 0, y:-130)
+                .foregroundColor(self.colorScheme == .dark ? Color.white : Color.black)
+            if (self.colorScheme == .dark ){
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color.white, lineWidth: self.colorScheme == .dark ? 2 : 0)
+                    .frame(width: 60, height: 295)
+            }else{
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 60, height: 305)
+                    .foregroundColor(Color.white)
+                    .shadow(color: Colors.LegendText, radius: 12, x: 0, y: 6 )
+                    .blendMode(.multiply)
+            }
+        }
+    }
+}
+
+struct MagnifierRect_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
