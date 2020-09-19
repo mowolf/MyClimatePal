6066dc6ad0d09ebae3f185ac01c03a43ef4180b7
Moritz Wolf <moritz.m.wolf@tum.de>
2020-09-19 20:11:15 +0200

    add tree view

--- myclimatepal/myclimatepal/model.swift
+++ myclimatepal/myclimatepal/model.swift
@@ -11,11 +11,10 @@ import Foundation
 import Fuse
 import SwiftUI
 
-//let co2val = 70.0
-//currentCo2State = co2val/co2max
 
 final class Co2State: ObservableObject {
     // MARK: Co2
+    @Published var trees: Int = 0
     @Published var currentCo2State: Double = 0.0
     @Published var co2max = 11.0
     @Published var co2HistoryData: [Double] = []//[8, 23, 54, 32, 12, 37, 7, 23, 43]
